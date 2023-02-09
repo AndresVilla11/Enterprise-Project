98011aaf2bf0f8cfbfbc5dd543522730bc7fea1b
Andres Villamizar <a.villa11@hotmail.com>
2023-02-09 17:38:36 -0500

    DataSource exclude

--- joiner/joiner/src/main/java/com/enterprise/joiner/JoinerMicroServiceApplication.java
+++ joiner/joiner/src/main/java/com/enterprise/joiner/JoinerMicroServiceApplication.java
@@ -2,8 +2,9 @@ package com.enterprise.joiner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class JoinerMicroServiceApplication {
 
 	public static void main(String[] args) {
