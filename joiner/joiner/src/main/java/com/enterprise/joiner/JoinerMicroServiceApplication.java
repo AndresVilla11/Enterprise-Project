309ec39e88dc57bb227a9ab8d211e11719bf29d7
Andres Villamizar <a.villa11@hotmail.com>
2023-02-08 20:40:44 -0500

    first commit

---
+++ joiner/joiner/src/main/java/com/enterprise/joiner/JoinerMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.joiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinerMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinerMicroServiceApplication.class, args);
+	}
+
+}
---
+++ joiner/joiner/src/test/java/com/enterprise/joiner/JoinerMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.joiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinerMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
