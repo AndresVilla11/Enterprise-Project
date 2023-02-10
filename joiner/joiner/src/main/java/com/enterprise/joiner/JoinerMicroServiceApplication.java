9aab82a5847d34eefd7640eb265a3a2a0d6f3247
Andres Villamizar <a.villa11@hotmail.com>
2023-02-09 19:05:41 -0500

    Removed space

--- joiner/joiner/src/main/java/com/enterprise/joiner/JoinerMicroServiceApplication.java
+++ joiner/joiner/src/main/java/com/enterprise/joiner/JoinerMicroServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class JoinerMicroServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JoinerMicroServiceApplication.class, args);
+		SpringApplication.run(JoinerMicroServiceApplication.class,args);
 	}
 
 }
