3122861eb2bd4cc4e18caecd30b50fbc59ba9d87
Andres Villamizar <a.villa11@hotmail.com>
2023-02-15 18:24:23 -0500

    Add pitest

--- joiner/joiner/src/main/java/com/enterprise/joiner/controller/JoinerController.java
+++ joiner/joiner/src/main/java/com/enterprise/joiner/controller/JoinerController.java
@@ -1,4 +1,21 @@
 package com.enterprise.joiner.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/joiners")
 public class JoinerController {
+    @PostMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createJoiner(@RequestBody String data) {
+        return "";
+    }
 }
